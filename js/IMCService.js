617eb83d60eac0e50ddd746494c6649c10c80e00
juliocezah <julim_cdo@hotmail.com>
2015-05-12 21:26:00 -0300

    Added logic calculate IMC

---
+++ js/IMCService.js
@@ -0,0 +1,12 @@
+var IMCService = {
+    
+    calculate: function(height, weight){
+        return weight / (height * height);
+    },
+    
+    //TODO to implemented
+    description: function(index){
+        return "To be implemented";
+    }
+
+};
